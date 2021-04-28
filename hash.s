d0497aa949e452cf855beca9c3ac3013e31b22ff
Ayush Agarwal <agarwalayush161@gmail.com>
2021-04-28 13:46:35 -0700

    Updated hash.S

--- hash.s
+++ hash.s
@@ -1,7 +1,5 @@
-// CES30 FP2float assignment template
+// CES30 assignment template
 // 
-// Your name:
-// your pid:
 //
 // Describe target Hardware to the assembler
 .arch   armv6
@@ -10,3 +8,32 @@
 /////////////////////////////////////////////////////////
 
 .text                       // start of the text segment
+/////////////////////////////////////////////////////////
+// function hashFun 
+/////////////////////////////////////////////////////////
+.type   hashFun, %function // define as a function
+.global hashFun            // export function name
+.equ    HF_OFF_HF, 28      // (regs - 1) * 4
+/////////////////////////////////////////////////////////
+
+// put any .equ for hashFun here - delete this line
+
+/////////////////////////////////////////////////////////
+
+hashFun:
+    push    {r4-r9, fp, lr}     // use r4-r9 protected regs
+    add     fp, sp, HF_OFF_HF  // locate our frame pointer
+    // do not edit the prologue above
+    // You can use temporary r0-r3 and preserved r4-r9
+    // Store return value (results) in r0
+    /////////////////////////////////////////////////////////
+
+    // put your code for hashFun here - delete this line
+
+    /////////////////////////////////////////////////////////
+    // do not edit the epilogue below
+    sub     sp, fp, HF_OFF_HF  // restore sp
+    pop     {r4-r9,fp, lr}      // restore saved registers
+    bx      lr                  // function return 
+    .size   hashFun,(. - hashFun)
+
